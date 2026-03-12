aee16fe7291992a7be4b4f336947146811ea393a
Claudiu Dascalu <claudiucris2002@gmail.com>
2026-03-12 16:39:29 +0200

    build(clean-architecture): add projects and folders for each part of the project

---
+++ BackendCleanArhitecture/LabResource.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LabResource.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LabResource.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+}
