ce6edfd556b3a86ca11a0fe3d9248461d06fe38f
Claudiu Dascalu <claudiucris2002@gmail.com>
2026-03-12 12:49:17 +0200

    build(clean-arhitecture): configure project and program.cs

---
+++ BackendCleanArhitecture/LabResource.CleanArhitecture/LabResource.CleanArhitectureAPI/Program.cs
@@ -0,0 +1,43 @@
+using FluentValidation;
+using Microsoft.OpenApi;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "LabResource Manager - Clean Arhitecture API",
+        Version = "v1",
+        Description = "API-ul pentru arhitectura Clean Arhitecture"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
