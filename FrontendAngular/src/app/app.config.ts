c6fa8e50f90fabeafb5a0de18fad4aadc0d04031
Claudiu Dascalu <claudiucris2002@gmail.com>
2026-04-01 19:45:07 +0300

    chore: setup PrimeNG, PrimeFlex, fonts and global theming

--- FrontendAngular/src/app/app.config.ts
+++ FrontendAngular/src/app/app.config.ts
@@ -1,11 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+// Importurile noi pentru PrimeNG
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 import { routes } from './app.routes';
+import { backendInterceptor } from './core/interceptors/backend-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([backendInterceptor])
+    ),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+          darkModeSelector: 'none' 
+      }
+    }
+    })
   ]
 };
