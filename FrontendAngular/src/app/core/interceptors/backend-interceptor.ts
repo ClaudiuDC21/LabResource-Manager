c1f2625ee509dba234e918ab268dc34a05e11d77
Claudiu Dascalu <claudiucris2002@gmail.com>
2026-04-27 18:30:40 +0300

    feat(frontend-auth): add 401 interceptor for auto logout

--- FrontendAngular/src/app/core/interceptors/backend-interceptor.ts
+++ FrontendAngular/src/app/core/interceptors/backend-interceptor.ts
@@ -1,28 +1,25 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { BackendConfigService } from '../services/backend-config';
+import { BackendConfigService } from '../services/backend-config.service';
 
 export const backendInterceptor: HttpInterceptorFn = (req, next) => {
   const backendConfig = inject(BackendConfigService);
   
-  // 1. Ignorăm fișierele locale (imagini SVG, assets)
   if (req.url.startsWith('./') || req.url.startsWith('assets/')) {
     return next(req);
   }
 
-  // 2. Construim URL-ul complet către backend (Clean sau Vertical)
   let apiReq = req.clone({
     url: `${backendConfig.currentUrl()}${req.url.startsWith('/') ? '' : '/'}${req.url}`
   });
 
-  // 3. Extragem token-ul din localStorage și îl punem în Header (AICI ERA PROBLEMA!)
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+  
   if (token) {
     apiReq = apiReq.clone({
       headers: apiReq.headers.set('Authorization', `Bearer ${token}`)
     });
   }
 
-  // 4. Trimitem request-ul final
   return next(apiReq);
 };
